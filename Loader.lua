57dd9c9a13a9c8cb27bda7bc4230581629be3912
Birthday X Scripts <87570691+BirthScripts@users.noreply.github.com>
2022-09-21 15:04:30 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BirthScripts/NerdV4/main/MainScript.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BirthScripts/Whitelist/main/load.lua"))
